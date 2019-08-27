72c7e01b16eb158fbac9d93f2cd5e1e7aa1aaa7b
Rupinder Nagra <rupinder0410@gmail.com>
2019-08-27 18:31:29 +0000

    fix header/footer to include css, add public directory for stylesheets

--- app.js
+++ app.js
@@ -10,33 +10,31 @@ const express = require('express'),
 // CONFIGURATION
 // =============
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
 
-
+// OBTAIN TODAY'S DATE FORMAT FOR API
 var today = new Date();
 var ddt = String(today.getDate()).padStart(2, '0');
 var mmt = String(today.getMonth() + 1).padStart(2, '0'); 
 var yyyyt = today.getFullYear();
-
 today = yyyyt + '-' + mmt + '-' + ddt;
-console.log(today);
 
+// OBTAIN PAST DATE (2 MONTHS PREVIOUS) FORMAT FOR API
 var past = new Date();
 var dd = String(past.getDate()).padStart(2, '0');
 var mm = String(past.getMonth() - 1).padStart(2, '0');
 var yyyy = past.getFullYear();
-
 past = yyyy + '-' + mm + '-' + dd;
-console.log(past);
 
-
-
+app.get('/', function(req, res){
+	res.render('home');
+});
 
 app.get("/main", function(req, res){
     var url = "https://api.themoviedb.org/3/discover/movie?api_key=57198b2c3e654b257b7cf99d000169d9&primary_release_date.gte=" + past + "&primary_release_date.lte=" + today;
     request(url, function(error, response, body){
         if(!error && response.statusCode == 200) {
             var data = JSON.parse(body)
-			console.log(data['results'][0]['vote_average']);
             res.render("main", {data: data});
         }
     });
