a1964f86e038f12d4f42a2c0014a47bbf78743f3
Rupinder Nagra <rupinder0410@gmail.com>
2019-08-27 20:33:07 +0000

    add login/register routes, format home page

--- app.js
+++ app.js
@@ -26,8 +26,14 @@ var mm = String(past.getMonth() - 1).padStart(2, '0');
 var yyyy = past.getFullYear();
 past = yyyy + '-' + mm + '-' + dd;
 
-app.get('/', function(req, res){
-	res.render('home');
+app.get("/", function(req, res){
+    var url = "https://api.themoviedb.org/3/discover/movie?api_key=57198b2c3e654b257b7cf99d000169d9&primary_release_date.gte=" + past + "&primary_release_date.lte=" + today;
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200) {
+            var data = JSON.parse(body)
+            res.render("home", {data: data});
+        }
+    });
 });
 
 app.get("/main", function(req, res){
@@ -38,6 +44,18 @@ app.get("/main", function(req, res){
             res.render("main", {data: data});
         }
     });
+});
+
+// app.get('/main/:id', function(req, res){
+// 	var 
+// });
+
+app.get('/login', function(req, res){
+	res.render('login');
+});
+
+app.get('/register', function(req, res){
+	res.render('register');
 });
 
 
