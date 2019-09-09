579233237674c82e993723f5127ac337cbface58
RupinderN <rupinder0410@gmail.com>
2019-09-09 04:37:35 +0000

    added login/register authentication

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-// ==============
-// REQUIRE ROUTES
-// ==============
+// ========
+// REQUIRE 
+// ========
 const express = require('express'),
 	  app = express(),
 	  request = require("request"),
 	  mongoose = require('mongoose'),
-	  bodyParser = require('body-parser');
+	  User = require('./models/user'),
+	  bodyParser = require('body-parser'),
+	  passport = require('passport'),
+	  LocalStrategy = require('passport-local'),
+	  passportLocalMongoose = require('passport-local-mongoose');
 
 
-// ==============
-// CONFIGURATION
-// ==============
-// mongoose.connect("mongodb://localhost:27017/movie_app", { useNewUrlParser: true });
+// ======================
+// GENERAL CONFIGURATION
+// ======================
+
+
+mongoose.connect("mongodb://localhost:27017/movie_app", { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+// =======================
+// PASSPORT CONFIGURATION
+// =======================
+
+
+app.use(require("express-session")({
+    secret: "I am a godly programmer",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// =======
+// ROUTES
+// =======
 
 
 app.get("/", function(req, res){
@@ -37,20 +68,49 @@ app.get("/main", function(req, res){
 	});
 });
 
+app.post('/more', function(req, res) {
+	var newMovie = req.body.movie;
+	res.render("show", {movie: newMovie});
+});
+
+
+// ======================
+// AUTHENTICATION ROUTES
+// ======================
+
+
 app.get('/login', function(req, res){
 	res.render('login');
+});
+
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
 });
 
 app.get('/register', function(req, res){
 	res.render('register');
 });
 
-app.post('/more', function(req, res) {
-	var newMovie = req.body.movie;
-	res.render("show", {movie: newMovie});
+app.post('/register', function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+			console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/main"); 
+        });
+	});
 });
+
 
 // ==============
 // SERVER STARTUP
 // ==============
+
 app.listen(3000, () => { console.log('Server has started!') });
+
